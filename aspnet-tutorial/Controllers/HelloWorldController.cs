877b19e28fd534d1c2db6c263f4f1b5de465672f
semagaroy <31117639+semagaroy@users.noreply.github.com>
2017-09-19 22:10:59 -0400

    Adding a View

--- aspnet-tutorial/Controllers/HelloWorldController.cs
+++ aspnet-tutorial/Controllers/HelloWorldController.cs
@@ -11,17 +11,20 @@ namespace aspnet_tutorial.Controllers
         // 
         // GET: /HelloWorld/ 
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
